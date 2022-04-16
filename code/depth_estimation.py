e57c9cf7c91b72de0e9d8aa26d89c5b30dc2c69f
Sumedh Reddy <sumedhreddykoppula@gmail.com>
2022-04-16 19:11:35 -0400

    Calculated Fundamental Matrix

--- code/depth_estimation.py
+++ code/depth_estimation.py
@@ -1,50 +1,100 @@
 from helper import *
 
 data_selection = input('\n 1. curule dataset \n 2. octagon dataset \n 3. pendulum dataset \n Please select the dataset to determine the depth estimate: ')
-#Defining Calibration data and baselines from given dataset
-if data_selection == 1: #curule dataset
+
+#selecting curule dataset
+if data_selection =='1': 
     print("Using 'curule' dataset...")
     cam0 = np.array([[1758.23, 0, 977.42], [0, 1758.23, 552.15], [0, 0, 1]])
     cam1 = np.array([[1758.23, 0, 977.42], [0, 1758.23, 552.15], [0, 0, 1]])
     baseline = 88.39  
     f = cam0[0,0]
-    path = '/home/starfleeet-robotics/Desktop/StereoVisionSystem/data/curule'
+    images_path = '/Users/sumedhreddy/Desktop/StereoVisionSystem/data/curule'
 
-elif data_selection == 2:   #octagon dataset
+#selecting octagon dataset
+elif data_selection =='2':   
     print("Using 'octagon' dataset...")
     cam0 = np.array([[1742.11, 0, 804.90], [0, 1742.11, 541.22], [0, 0, 1]])
     cam1 = np.array([[1742.11, 0, 804.90], [0, 1742.11, 541.22], [0, 0, 1]])
     baseline = 221.76 
     f = cam0[0,0]
-    path = '/home/starfleeet-robotics/Desktop/StereoVisionSystem/data/octagon'
+    images_path = '/Users/sumedhreddy/Desktop/StereoVisionSystem/data/octagon'
 
-elif data_selection == 3:   #pendulum dataset
+#selecting pendulum dataset
+elif data_selection =='3':
     print("Using 'pendulum' dataset...")
     cam0 = np.array([[1729.05, 0, -364.24], [0, 1729.05, 552.22], [0, 0, 1]])
     cam1 = np.array([[1729.05, 0, -364.24], [0, 1729.05, 552.22], [0, 0, 1]])
     baseline = 537.75
-    print(baseline)
     f = cam0[0,0]
-    path = '/home/starfleeet-robotics/Desktop/StereoVisionSystem/data/pendulum'
+    images_path = '/Users/sumedhreddy/Desktop/StereoVisionSystem/data/pendulum'
 
 
 # Calibrating the selected dataset
 print("Reading images from the selected dataset...")
-images = dataLoader(path)
-print(images)
+images = dataLoader(images_path)
+# Feature Matching, Fundamental Matrix and RANSAC
 # sift_image for feature matching
 sift_image = cv2.xfeatures2d.SIFT_create()
 image0 = images[0].copy()
 image1 = images[1].copy()
 
-# Convert Images to Grayscale
+# Convert the input images to Grayscale
 image0_gray = cv2.cvtColor(image0, cv2.COLOR_BGR2GRAY) 
 image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
-print("Finding matches between images using sift_image...")
-kp1, des1 = sift_image.detectAndCompute(image0_gray, None)
-kp2, des2 = sift_image.detectAndCompute(image1_gray, None)
-bf = cv2.BFMatcher()
-matches = bf.match(des1,des2)
-matches = sorted(matches, key = lambda x :x.distance)
-chosen_matches = matches[0:100]
+print("Finding matches between two images using sift_image")
+# Finding the Matching points between two images
+kp1, des_1 = sift_image.detectAndCompute(image0_gray, None)
+kp2, des_2 = sift_image.detectAndCompute(image1_gray, None)
+matcher = cv2.BFMatcher()
+matched = matcher.match(des_1,des_2)
+# sorting matched points
+matched = sorted(matched, key = lambda x :x.distance)
+# choosing 0:100
+selected_matches = matched[0:100]
+features = []
+for i, mth in enumerate(selected_matches):
+    point1 = kp1[mth.queryIdx].pt
+    point2 = kp2[mth.trainIdx].pt
+    features.append([point1[0], point1[1], point2[0], point2[1]])
+    
+features = np.array(features).reshape(-1, 4)
+# Match Outlier Rejection via RANSAC
+# Get Inliner RANSAC 
+# Calculating F matrix and obtaining maximized number of inliers
+
+required_idx = []
+Fundamental_matrix = 0
+iterations = 1000
+inliers_threshold = 0
+error_threshold = 0.03
+
+for i in range(0, iterations):
+    idxs = []
+    # choosing 8 correspondences randomly
+    n_rows = features.shape[0]
+    random_idx = np.random.choice(n_rows, size=8)
+    point_8_features = features[random_idx, :] 
+    x_1 = point_8_features[:,0:2]
+    x_2 = point_8_features[:,2:4]
+    # Calculating Fundamental Matrix
+    F = calcFundamentalMatrix(x_1, x_2)
+    for j in range(n_rows):
+        feature = features[j]
+        x1,x2 = feature[0:2], feature[2:4]
+        x1_tmp=np.array([x1[0], x1[1], 1]).T
+        x2_tmp=np.array([x2[0], x2[1], 1])
+        err = np.dot(x1_tmp, np.dot(F, x2_tmp))
+        err = np.abs(err)
+        if err < error_threshold:
+            idxs.append(j)
+
+    if len(idxs) > inliers_threshold:
+        inliers_threshold = len(idxs)
+        required_idx = idxs
+        Fundamental_matrix = F
+
+# Matched Inliners
+matched_inliers = features[required_idx, :]
+print("Fundamental Matrix : ", Fundamental_matrix)
--- code/helper.py
+++ code/helper.py
@@ -10,13 +10,58 @@ from numpy import histogram_bin_edges, linalg as LA
 from os.path import isfile, join
 
 
+def calcFundamentalMatrix(x_1, x_2):
+    normalization = True
+    if x_1.shape[0] > 7:
+        if normalization == True:
+            x1_norm, T1 = calcNormalization(x_1)
+            x2_norm, T2 = calcNormalization(x_2)
+        else:
+            x1_norm,x2_norm = x_1,x_2
+            
+        A = np.zeros((len(x1_norm),9))
+        for i in range(0, len(x1_norm)):
+            x_1,y_1 = x1_norm[i][0], x1_norm[i][1]
+            x_2,y_2 = x2_norm[i][0], x2_norm[i][1]
+            A[i] = np.array([x_1*x_2, x_2*y_1, x_2, y_2*x_1, y_2*y_1, y_2, x_1, y_1, 1])
+
+        # Calculating SVD 
+        U, S, VT = np.linalg.svd(A, full_matrices=True)
+        F = VT.T[:, -1]
+        F = F.reshape(3,3)
+        u, s, vt = np.linalg.svd(F)
+        s = np.diag(s)
+        s[2,2] = 0
+        F = np.dot(u, np.dot(s, vt))
+        if normalization:
+            F = np.dot(T2.T, np.dot(F, T1))
+        return F
+        
+    else:
+        return None
+
+def calcNormalization(uv):
+    
+    uv_d = np.mean(uv, axis=0)
+    u_d ,v_d = uv_d[0], uv_d[1]
+
+    u_cap = uv[:,0] - u_d
+    v_cap = uv[:,1] - v_d
+
+    s = (2/np.mean(u_cap**2 + v_cap**2))**(0.5)
+    T_scale = np.diag([s,s,1])
+    T_trans = np.array([[1,0,-u_d],[0,1,-v_d],[0,0,1]])
+    T = T_scale.dot(T_trans)
+    x_ = np.column_stack((uv, np.ones(len(uv))))
+    x_norm = (T.dot(x_.T)).T
+
+    return  x_norm, T
+
 def dataLoader(path):
-    print("loading images from ", path)
     images_data = []
     for n in range(0, 2):
         imgs = path + "/" + "im" + str(n) + ".png"
         image = cv2.imread(imgs)
-        
         if image is not None:
             images_data.append(image)			
         else:
